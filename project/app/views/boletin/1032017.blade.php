a6d9709c673c761ba243a54dee66a9f2f3440ecd
Carlucho <shenlong_12@hotmailcom>
2017-03-11 22:47:12 -0400

    boletin link added

---
+++ project/app/views/boletin/1032017.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Boletin | Fundaepekeina</title>
+    <link href="http://fundaepekeina.org/html/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+              
+    <link href="http://fundaepekeina.org/html/css/main.css" rel="stylesheet">
+    <link href="http://fundaepekeina.org/html/css/responsive.css" rel="stylesheet">
+    <link href="http://fundaepekeina.org/css/custom.css" rel="stylesheet">
+    <style type="text/css">
+        .img-boletin
+        {
+            max-height: 312px;
+        }
+
+    </style>
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+<!--<div class="logo-image" style="margin-bottom: 50px;">
+    <a href="index.html"><img class="img-responsive" src="http://fundaepekeina.org/images/logo.png" alt=""> </a>
+</div>-->
+<section id="" class="boletin">
+    <div class="">
+        <div class="row" style="margin-bottom: 50px">
+            <img src="http://fundaepekeina.org/images/boletin/btn_cabeza.jpg" class="img-responsive center-block">
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-9 fixedheight">
+                <div class="col-xs-12 bg bg-blue formulario">
+                    <img src="http://fundaepekeina.org/images/news/1489003594.jpg" class="img-responsive center-block img-boletin">
+                    <div class="content">
+                        <h2 class="boletin-title">ULA y Escuela de Campo La Laguna graduaron a 37 caficultores</h2>
+                        <p class="text-justify">Febrero marcó el éxito de la Escuela de Campo La Laguna. Treinta
+                            y siete caficultores fueron diplomados por la Universidad de Los Andes (ULA) por
+                            convenio suscrito con la Fundación Epékeina, junto al Programa de Pequeñas
+                        Donaciones del Fondo para el Medio Ambiente Mundial (FMAM) y el Program [...]</p>
+                        <a target="_blank" href="http://fundaepekeina.org/noticias/256" class="btn btn-default btn-xs pull-right">Leer más</a>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 bg bg-green formulario fixedHeight">
+                        <img src="http://fundaepekeina.org/images/news/1489003834.jpg" class="img-responsive center-block img-boletin" alt="Celebración de 7 años de FE">
+                        <div class="content">
+                            <h2 class="boletin-title">Celebración de 7 años de FE</h2>
+                            <p class="text-justify">En Funda Epékeina (FE)
+                                festejamos nuestros primeros 7 años el sábado 25 de febrero de 2017 con una
+                                fiesta en la que se encontraron Sede Las Acacias y Sede La Boyera. Fue un día
+                                maravilloso, muy divertido y enriquecedor, como cada jornada que hacemos.
+                            Resultó una oportunidad muy especial par [...]</p>
+                            <a target="_blank" href="http://fundaepekeina.org/noticias/257" class="btn btn-default btn-xs pull-right">Leer más</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6 bg bg-yellow formulario fixedHeight">
+                        <img src="http://fundaepekeina.org/images/news/1489004003.jpg" class="img-responsive center-block img-boletin" alt="Leer con gusto y algo más">
+                        <div class="content">
+                            <h2 class="boletin-title">Leer con gusto y algo más</h2>
+                            <p class="text-justify">A
+                                partir de enero, el desayuno que ofrecemos a nuestros niños y adolescentes en
+                                Sede La Boyera es doblemente nutritivo. Además de disfrutar de una comida
+                                sustanciosa e hidratación sana, reciben alimento de palabras, conocimiento, entretenimiento,
+                            entre otros, a través de la dinámica “Leer [...]</p>
+                            <a target="_blank" href="http://fundaepekeina.org/noticias/258" class="btn btn-default btn-xs pull-right">Leer más</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6 bg bg-pink formulario fixedHeight">
+                        <img src="http://fundaepekeina.org/images/news/1489004463.jpg" class="img-responsive center-block img-boletin" alt="Red Joven Venezuela recibió Orden José Félix Ribas en Mérida">
+                        <div class="content">
+                            <h2 class="boletin-title">Red Joven Venezuela recibió Orden José Félix Ribas en Mérida</h2>
+                            <p class="text-justify">Con motivo a la celebración de los 203 años de la Batalla de
+                                La Victoria y Día de la Juventud, el Concejo Municipal de Libertador en el
+                                estado Mérida, otorgó la Orden José Félix Ribas
+                                en su única clase, a la Red Joven Venezuela, por el trabajo que viene
+                            desarrollando a favor de la juven [...]</p>
+                            <a target="_blank" href="http://fundaepekeina.org/noticias/261" class="btn btn-default btn-xs pull-right">Leer más</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>                                                                                                              
+                    <div class="col-xs-12 col-md-6 bg bg-blue formulario fixedHeight">
+                        <img src="http://fundaepekeina.org/images/news/1489004166.jpg" class="img-responsive center-block img-boletin" alt="Visita de Ministros de la Palabra">
+                        <div class="content">
+                            <h2 class="boletin-title">Visita de Ministros de la Palabra</h2>
+                            <p class="text-justify">Cada
+                                mes, nuestro programa Epékeina Sacerdotal dedica un sábado a visitar a los
+                                sacerdotes retirados que residen en la Casa Sacerdotal de la Arquidiócesis de
+                                Caracas y de la Comunidad de las Hermanas Siervas de Jesús en Caracas. Ello se
+                            realiza con el apoyo de los diversos apostolados de la  [...]</p>
+                            <a target="_blank" href="http://fundaepekeina.org/noticias/259" class="btn btn-default btn-xs pull-right">Leer más</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>                                                                                                                      
+                </div>
+            </div>   
+            <div class="col-xs-12 col-md-3">
+                <a href="http://fundaepekeina.org/contacto/donaciones"><img src="http://fundaepekeina.org/images/boletin/btn_dona.jpg" class="img-responsive" style="width:100%;"></a>
+                <div class="social-container">
+                    <h3>Síguenos en:</h3>
+                    <hr>
+                    <ul>
+                        <li><a href="https://twitter.com/fundaepekeina"><i class="fa ion-social-twitter"></i></a></li>
+                        <li><a href="http://fundaepekeina.org"><i class="fa ion-social-instagram-outline"></i></a></li>
+                        <li><a href="https://www.facebook.com/funda.epekeina"><i class="fa ion-social-facebook"></i></a></li>
+                        <li><a href="https://www.youtube.com/user/fundaepekeina"><i class="fa ion-social-youtube"></i></a></li>
+                    </ul>
+                </div>
+                <br>
+                <h2 class="text-blue">Historias Epékeinas</h2>
+                <br>
+                <img src="http://fundaepekeina.org/images/news/1489004282.jpg" class="img-responsive" alt="María Jesús Zerolo: Hada Madrina que abre su corazón a los más necesitados">
+                <div class="bg-green">
+                    <h2 class="hist-title">María Jesús Zerolo: Hada Madrina que abre su corazón a los más necesitados.</h2>
+                </div>
+                <hr>
+                <div class="text-justify">
+                    María Jesús Zerolo, una loable dama a quien le inquietan  las necesidades básicas de los niños y que, sin recibir nada a cambio, se ocupa de solventarlas, en especial de aquellos que residen en la zona que la vio crecer: Las Acacias.
+                    <br>
+                    <br>
+                    
+                    Por su capacidad de resolver las dificultades y su disponibilidad incondicional, se ha ganado el reconocimiento de todos y en especial, el título de “Hada Madrina”, en virtud de que el reloj y el calendario, no han sido impedimentos para que María Jesús comparta, colabore y contribuya a regalar sonrisas.
+                    <br>
+                    <br>
+
+                    Desde los inicios de Funda Epékeina ha colaborado con las actividades ideadas para el bienestar de sus beneficiarios. Su inspiración son los pequeños y el equipo de trabajo que con mucho cariño, empeño y esfuerzo llevan adelante la organización. “El haberlos conocido fue mi gran motivación. Llegué por invitación de una voluntaria y me he quedado con mucho gusto durante siete años”, relató.
+                    <br>
+                    <a href="http://fundaepekeina.org/quienes-somos/historias-epekeinas/260" class="pull-right">Leer más</a>
+                </div>
+            </div>
+        </div>
+        <section id="coming-soon-footer" class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="text-center">
+                        <p>&copy; Derechos Reservados Funda Epékeina 2016.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="row" style="margin:0;">
+            <div class="col-xs-3 bg-square bg-blue">
+            </div>
+            <div class="col-xs-3 bg-square bg-yellow">
+            </div>
+            <div class="col-xs-3 bg-square bg-green">
+            </div>
+            <div class="col-xs-3 bg-square bg-pink">
+            </div>
+        </div>
+
+    </div>
+</section>
+    
+</body>
+</html>
---
+++ project/app/views/boletin/922017.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Boletin | Fundaepekeina</title>
+    <link href="http://fundaepekeina.org/html/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://fundaepekeina.org/html/css/font-awesome.min.css" rel="stylesheet"> 
+    <link href="http://fundaepekeina.org/html/css/main.css" rel="stylesheet">
+    <link href="http://fundaepekeina.org/html/css/responsive.css" rel="stylesheet">
+    <link href="http://fundaepekeina.org/css/custom.css" rel="stylesheet">
+    <style type="text/css">
+        @font-face{
+           font-family: 'TypoGraphica';
+           src: url('http://fundaepekeina.org/fonts/TypoGraphica.otf');
+        }
+        html *
+        {
+            font-family: 'TypoGragica' !important;
+        }
+        .img-boletin
+        {
+            max-height: 312px;
+        }
+        .veronica
+        {
+            font-size: 44px;
+        }
+
+    </style>
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="http://fundaepekeina.org/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://fundaepekeina.org/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://fundaepekeina.org/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://fundaepekeina.org/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="http://fundaepekeina.org/images/ico/apple-touch-icon-57-precomposed.png">
+    <style type="text/css">
+        .img-text-right, .img-text-left
+        {
+            border: 20px solid #25aae2;
+            width: 100%;
+        }
+        .bg-square
+        {
+            min-height: 30px;
+        }
+        .bg-blue
+        {
+            background-color: #25aae2;
+        }
+        .bg-yellow
+        {
+            background-color: #ffca08;
+        }
+        .bg-green
+        {
+            background-color: #a6ce39;
+        }
+        .bg-pink
+        {
+            background-color: #ed008c;
+        }
+        .text-blue
+        {
+            color: #25aae2; 
+        }
+        .text-green
+        {
+            color: #a6ce39;
+        }
+        .text-pink
+        {
+            color: #ed008c;
+        }
+        .bolder
+        {
+            font-weight: 900;
+        }
+    </style>
+</head><!--/head-->
+
+<body>
+    <!--<div class="logo-image" style="margin-bottom: 50px;">                                
+       <a href="index.html"><img class="img-responsive" src="{{ asset('images/logo.png') }}" alt=""> </a> 
+    </div>-->
+     <section id="">        
+         <div class="container">
+            <div class="row" style="margin-bottom: 50px">
+                <img src="http://fundaepekeina.org/images/slides/boletin_banner.jpg" class="img-responsive center-block">
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-6">
+                    <p class="text-justify">
+                        <img src="http://fundaepekeina.org/images/info/nuestros.jpg" class="img-responsive">
+                        Contar la historia de Funda Epékeina es como relatar la vida de una persona: nacer, descubrir lo incierto, dar los primeros pasos, caer y levantarse, aprender a hablar, vivir alegrías, superar obstáculos y asumir las transformaciones propias del crecimiento. Y eso ha sido este recorrido de nuestros primeros 7 años: un constante desarrollo que se refleja en beneficiarios, voluntarios y directiva.
+                        <br>    
+                        <br>
+
+                        ¡Cuánto hemos crecido! Nos ponemos nostálgicos revisando las fotos de aquel 2010 cuando toda esta historia comenzó y al ver qué grandes están nuestros beneficiarios –algunos ya convertidos en voluntarios-. Son innumerables los recuerdos de las actividades desarrolladas con amor, compromiso y responsabilidad.
+                        <br>
+                        <br>
+
+                        En estos 7 años, hemos avanzado con constancia y determinación en el cumplimiento de nuestros objetivos bajo el lema “Enseñando valores, formamos ciudadanos”. 
+                        <br>
+                        <br>
+                    </p>
+                </div>
+                <div class="col-xs-12 col-md-6"></div>
+                    <p class="text-justify">
+                        Hemos sumado voluntades y creado nuevos proyectos con los cuales hemos ampliado nuestro ámbito de acción en el territorio venezolano. Los programas infantiles de Las Acacias y La Boyera continúan creciendo, el Campamento Epékeinando próximamente ofrecerá su 8va. edición, los jóvenes se ven involucrados en Red Joven Venezuela en 13 estados, Epékeina Sacerdotal mantiene el acompañamiento a sacerdotes ancianos, HabitARTE sigue expresando valores en murales, hemos volado Papagayos por la Paz en cielo venezolano y muy pronto veremos la primera cosecha de café de sombra resultado del Proyecto de “Conservación del Paisaje Productivo La Comunidad La Laguna” .  
+                        <br>
+                        <br>
+
+                        Celebramos nuestros primeros 7 años aclamando un profundo agradecimiento a todos los que han estado ayudándonos a crecer. El equipo epékeino enaltece los valores que son la base de Funda Epékeina. Con cada acción que realizamos procuramos contribuir con el desarrollo de Venezuela en un contexto de justicia, paz y democracia. Aspiramos a perseverar en el acompañamiento de las comunidades para generar impactos productivos en niños, jóvenes y adultos.
+                        <br>
+                        <br>
+
+                        ¡Son nuestros primeros 7 años y, con la gracia de Dios, serán muchos más!
+                    </p>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 40px;">
+                    <img src="http://fundaepekeina.org/images/info/Boletin_FEB-con-gracia-de-Dios.jpg" class="img-responsive">
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <img src="http://fundaepekeina.org/images/info/historia.jpg" class="img-responsive">
+                    <h1 class="veronica">
+                        <span class="text-pink"><strong>Verónica Macedo,</strong></span>
+                    </h1>   
+                    <h2>
+                        <span class="text-blue"><strong>gota de vida y acción de <span class="bolder">Funda Epékeina</span></strong></span>
+                    </h2>
+                    <p class="text-justify">
+                        Su nombre es Verónica Macedo, en su historia de vida guarda apreciadas vivencias cargadas de risas, alegrías y en el fondo, mucha nostalgia. Su historia tomó un giro hace 7 años, inicios del 2010, cuando en compañía del padre Honegger Molina, párroco de la parroquia El Salvador en Las Acacias, para ese entonces, iniciaron labores en la Fundación Epékeina. Su deseo de hacer el bien, ofrecer protección al desasistido y contribuir con el bienestar de los más pequeños, sirvió para forjar triunfos en una de las zonas más vulnerables de Caracas: San Agustín del Sur. 
+                        <br>
+                        <br>
+                        Verónica se abrazó y dedicó al voluntariado para alcanzar su anhelo. Califica los inicios como “rudos”, en virtud de que eran más los deseos, los proyectos y los sueños, que el recurso humano. Con el interés de ir creciendo y alcanzar éxitos, relató con mucho entusiasmo que, el padre Honegger buscó ayuda con estudiantes del Liceo Padre Machado de Fe y Alegría. “Fuimos sumando, creciendo y dándole cuerpo a la misión con la que nació Funda Epékeina”. 
+                        <br>
+                        <br>
+
+                        Su buen accionar le permitió conquistar la ayuda de los voluntarios y poco a poco fueron logrando la atención de niños y jóvenes en el sector Terrazas del Alba, San Agustín del Sur. A Verónica le resulta gratificante que en corto tiempo agruparan a más de 33 niños. Eso, hace que se emocione cuando habla y recuerda cada una de las historias y gratos momentos que les dejaron sus “hijos”. Pues, así se refiere al hablar de quienes de forma recíproca conjugaron valores como amor, solidaridad y respeto. 
+                        <br>
+                        <br>
+                        “Fueron dos años y medio de un maravilloso trabajo. Hacía de todo. Con dedicación y mucho cariño fui catequista, recreadora, organizaba juegos, enseñé a leer la Biblia y en lo posible les ofrecíamos alimentos a los niños”, relató. Las actividades las desarrollaba todos los sábados, desde las 8:00 de la mañana hasta el mediodía, en el estacionamiento de una de las Torres de Terrazas del Alba. 
+                    </p>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <p class="text-justify">
+                        <img src="http://fundaepekeina.org/images/news/1486566347.jpg" class="img-responsive center-block img-text-right">
+                        Las anécdotas que vivió en su paso por Funda Epékeina son muchas. Sin embargo, con entusiasmo destacó aquella mañana en la que se encontraban descansando de una actividad física. “Estaba sentada recostada a una pared, rodeada por todos los niños”, pero una de las consentidas, Karyerit de nueve añitos, quien estaba un poco lejos, pero muy pendiente de su “mamá” -como le llamaba-, se acercó, y de forma habilidosa rasgó con ayuda de un huequito toda la parte trasera de la camisa de Verónica. Con muchas risas contó que “lo hizo por celos y tuve que regresar medio descubierta a casa”. 
+
+                        Emociona escucharla hablar de cada momento vivido y en específico, de sus estrategias de acción: abrazos, cariños, besos, escuchar, aconsejar y el hacerlos sentir importantes. Su lema de vida es aportar un granito de arena para los más necesitados. Por ello, considera que ofrecer esperanzas a quien no las tiene o no las conoce, es lo más gratificante. Funda Epékeina es para Verónica crecimiento personal y espiritual, o bien como lo definió, “llegar donde uno no cree que puede llegar”. 
+                        <br>
+                        <br>
+                        Por razones personales, actualmente reside en la ciudad de Puerto Ordaz. Mantiene contacto con miembros de la fundación y anhela volver a reunirse con sus “hijos” y alimentar su alma con las 32 sonrisas que logró categorizar. “Los amo, y si Dios me lo permitiera volvería a la Fundación”, enfatizó Verónica al narrar su historia Epékeina.
+                    </p>
+                </div>
+                <div class="col-xs-12">
+                    <div class="text-center">
+                        <h2 class="text-blue"><storng>Estén atentos a nuestra próxima entrega. Gracias.</storng></h2>
+                    </div>
+                </div>
+            </div>
+        </div>       
+    </section>
+
+    <section id="coming-soon-footer" class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="text-center">
+                        <p>&copy; Derechos Reservados Funda Epékeina 2016.</p>
+                </div>
+            </div>
+        </div>       
+    </section>
+    <div class="row">
+        <div class="col-xs-3 bg-square bg-blue">
+        </div>
+        <div class="col-xs-3 bg-square bg-yellow">
+        </div>
+        <div class="col-xs-3 bg-square bg-green">
+        </div>
+        <div class="col-xs-3 bg-square bg-pink">
+        </div>
+    </div>
+    
+</body>
+</html>
