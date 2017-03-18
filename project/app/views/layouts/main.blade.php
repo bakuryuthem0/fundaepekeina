003bd3690d30981c8b2b713621fbe0ad63d16ed2
Carlucho <shenlong_12@hotmailcom>
2017-03-18 13:23:54 -0400

    history added

--- project/app/views/layouts/main.blade.php
+++ project/app/views/layouts/main.blade.php
@@ -78,7 +78,8 @@
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right navbar-menu">
-                        <li class="@if($active == 'inicio') active @endif"><a href="{{ URL::to('/') }}">Inicio</a></li>
+                        <li class="@if($active == 'home') active @endif"><a href="{{ URL::to('quienes-somos/historias-epekeinas') }}">Historias Epékeinas</a>
+                        </li>
                         <li class="@if($active == 'about') active @endif dropdown">
                             <a href="{{ URL::to('quienes-somos') }}" class="visible-md-block visible-lg-block">Quiénes somos <i class="fa fa-angle-down"></i></a>
                             <a href="#!" class="hidden-md hidden-lg">Conócenos <i class="fa fa-angle-down"></i></a>
@@ -89,7 +90,9 @@
                                 <!--<li><a href="localizacion">¿Donde estamos?</a></li>-->
                                 <li><a href="{{ URL::to('organigrama') }}">Organigrama</a></li>
                             </ul>
-                        </li>                    
+                        </li>                   
+
+                        <!--<li><a href="{{ URL::to('quienes-somos/historias-epekeinas') }}">Historias Epékeinas</a></li> -->
                         <li class="@if($active == 'noticias') active @endif dropdown">
                             <a href="{{ URL::to('noticias')}}" class="visible-md-block visible-lg-block">Noticias <i class="fa fa-angle-down"></i></a>
                             <a href="#!" class="hidden-md hidden-lg">Noticias <i class="fa fa-angle-down"></i></a>
