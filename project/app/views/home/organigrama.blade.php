eded7b5d32724a642a74420318e96d02f8a43bf0
Carlucho <shenlong_12@hotmailcom>
2017-12-07 20:30:07 -0400

    lang text added

--- project/app/views/home/organigrama.blade.php
+++ project/app/views/home/organigrama.blade.php
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="">
                         <div class="col-sm-12">
-                            <h1 class="title">{{ Lang::get('lang.organization_chart') }}</h1>
+                            <h1 class="title">{{ Lang::get('lang.our_organization') }}</h1>
                         </div>                        
                     </div>
                 </div>
