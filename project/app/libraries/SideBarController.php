7bf1ee6f204f8a4d7c8cbdb77edb9f73333adb8d
Carlucho <shenlong_12@hotmailcom>
2018-01-06 19:53:50 -0400

    sideBar edited

--- project/app/libraries/SideBarController.php
+++ project/app/libraries/SideBarController.php
@@ -1,19 +1,31 @@
 <?php
 	Class SideBarController{
-		public static function getCategories($menu)
+		public static function getMostViewed($id = null)
 		{
-		    if ($menu != "all") {
-				$cat = Categoria::with(array('tipos' => function($tipos){
-					$tipos->with('slugs')
-					->with('descriptions');
-				}))->where('tipo','=',$menu)->get();
-		    }else
-		    {
-				$cat = Categoria::with(array('tipos' => function($tipos){
-					$tipos->with('slugs')
-					->with('descriptions');
-				}))->get();
-		    }
-			return $cat;
+			$articles = Visitor::groupBy('article_id')
+			->take(3)
+			->orderBy('top_articles','DESC')
+			->with(array('article' => function($article){
+				$article->with('likesCount')
+				->with('visitsCount')
+				->with('slugs')
+				->with('titles');
+			}))
+			->selectRaw('count(article_id) as top_articles, article_id')->get();
+			return $articles;
+		}
+		public static function getMostPopulars($id = null)
+		{
+			$articles = Like::groupBy('articulo_id')
+			->take(3)
+			->orderBy('top_articles','DESC')
+			->with(array('article' => function($article){
+				$article->with('likesCount')
+				->with('visitsCount')
+				->with('slugs')
+				->with('titles')->with('descriptions');
+			}))
+			->selectRaw('count(articulo_id) as top_articles, articulo_id')->get();
+			return $articles;
 		}
 	}
